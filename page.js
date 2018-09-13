f077e097fe830238e7c27fd944bcf6b8c724a627
josh <joshpeterson6.0@gmail.com>
2018-09-13 08:43:15 -0600

    Update page.js

--- page.js
+++ page.js
@@ -6,12 +6,41 @@ function whatPage() {
 	//var pathName = window.location.pathname;
 	var regex = /(?:https?:\/\/)(?:adminv2\.bitclubnetwork\.com)(\/support\-tickets)(\/\d+$)?/;
 	var match = host.match(regex);
+	var pageInt;
 
-	if (match[2] == null) {
-		alert("looks like ticket view");
-	} else if (match.length == 3) {
-		alert("looks like member view");
-	} else {
-		alert("looks like you messed up");
-	}
+			if (match[2] == null) {
+				//Ticket View
+				try {
+					clearInterval(pageInt);
+				} finally {
+					pageInt = setInterval(findr,1000);
+				}
+			} else if (match.length == 3) {
+				//Ticket View
+				try {
+					clearInterval(pageInt);
+				} finally {
+					pageInt = setInterval(memberRank);
+				}
+			} else {
+				alert("looks like you messed up");
+			}
+		
+
 }
+
+
+
+
+//var ripInt = setInterval(ripper, 1000);
+
+
+
+
+
+//set interval for page checker
+//page checker sets method intervals
+//how to clear method intervals on page change
+
+
+
